c75c47424d3a6ffa8eb499f35afe9def729ef299
vphuc0601 <nguyenvanphuc0601@gmail.com>
2021-06-06 20:39:28 +0700

    lucky

--- 03_Angular_Overview/Bai_tap/caculator-app/src/app/calculator/calculator.component.ts
+++ 03_Angular_Overview/Bai_tap/caculator-app/src/app/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {error} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-  num1 = 0;
-  num2 = 0;
+  num1: number;
+  num2: number;
   operator: string;
-  result: number;
+  result: any;
   constructor() { }
 
   ngOnInit(): void {
@@ -26,7 +27,12 @@ export class CalculatorComponent implements OnInit {
         this.result = this.num1 + this.num2;
         break;
       case '/':
-        this.result = this.num1 / this.num2;
+        if ( this.num2 === 0){
+          // error('Number 2 must not be 0');
+          this.result = 'Khong the chia cho 0';
+        }else {
+          this.result = this.num1 / this.num2;
+        }
         break;
     }
   }
